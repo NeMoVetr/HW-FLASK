10cd4713f97c620730f06072fcaa827dac8d7821
NeMoVetr <131559606+NeMoVetr@users.noreply.github.com>
2024-02-10 21:38:14 +0300

    ax

--- myproject2/myapp2/models.py
+++ myproject2/myapp2/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     added_date = models.DateField(auto_now_add=True)
+    photo = models.ImageField(upload_to='', null=True, blank=True)
 
     def __str__(self):
         return self.name
